65774316d0f3aa1374a508df6c066a01aef23b03
Alexey Gladkov <alexeygl@Metropolia.fi>
2026-08-26 14:04:45 +0300

    fix

---
+++ Tehtävä 3/4.py
@@ -0,0 +1,4 @@
+kanta = float(input('Anna suorakulmion kanta: '))
+korkeus = float(input('Anna suorakulmion korkeus: '))
+pintaala = kanta * korkeus
+print(f'Suorakulmion pinta-ala on {pintaala:.2f}')
