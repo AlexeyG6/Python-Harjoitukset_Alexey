6a6faced9740b5f8a33eaf855d2d78ea8e0078de
Alexey Gladkov <alexeygl@Metropolia.fi>
2026-08-26 14:11:15 +0300

    tehtävä 3-4

--- Tehtävä 3/3.py
+++ Tehtävä 3/3.py
@@ -1,4 +1,6 @@
 kanta = float(input('Anna suorakulmion kanta: '))
 korkeus = float(input('Anna suorakulmion korkeus: '))
 pintaala = kanta * korkeus
+piiri = 2 * (kanta + korkeus)
 print(f'Suorakulmion pinta-ala on {pintaala:.2f}')
+print(f'Suorakulmion piiri on {piiri:.2f}')
--- Tehtävä 3/4.py
+++ Tehtävä 3/4.py
@@ -1,4 +1,9 @@
-kanta = float(input('Anna suorakulmion kanta: '))
-korkeus = float(input('Anna suorakulmion korkeus: '))
-pintaala = kanta * korkeus
-print(f'Suorakulmion pinta-ala on {pintaala:.2f}')
+luku1 = float(input('Anna ensimmäinen luku: '))
+luku2 = float(input('Anna toinen luku: '))
+luku3 = float(input('Anna kolmas luku: '))
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = (luku1 + luku2 + luku3) / 3
+print(f'Lukujen summa on {summa:.2f}')
+print(f'Lukujen tulo on {tulo:.2f}')
+print(f'Lukujen keskiarvo on {keskiarvo:.2f}')
