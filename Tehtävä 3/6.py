d3263d2976ef635cd267447a6993f6874c7472e3
Alexey Gladkov <alexeygl@Metropolia.fi>
2026-08-26 17:28:59 +0300

    tenhtävä 3-6

---
+++ Tehtävä 3/6.py
@@ -0,0 +1,8 @@
+import random
+
+
+num1 = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+num2 = str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9))
+
+print("Kolminumeroinen luku:", num1)
+print("Nelinumeroinen luku:", num2)
