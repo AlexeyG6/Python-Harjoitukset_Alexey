0870e4f68835a6f9f1a6b4a8ea9f2c8ea0504209
Alexey Gladkov <alexeygl@Metropolia.fi>
2026-09-18 11:36:44 +0300

    Osio 10

--- Osio 10/1.py
+++ Osio 10/1.py
@@ -1,13 +1,11 @@
 
 class Hissi:
 
-    def __init__(self, alin, ylin):
-        self.alin = alin
-        self.ylin = ylin
+    def __init__(self):
         self.kerros = self.alin
 
     def siirry_kerrokseen(self, kerros):
-        while self.kerros == kerros:
+        while self.kerros != kerros:
             if self.kerros < kerros:
                 self.kerros_ylos()
             elif self.kerros > kerros:
@@ -22,7 +20,22 @@ class Hissi:
         self.kerros -= 1
         print(f"Hissi on kerroksessa {self.kerros}")
 
-h = Hissi(0, 5)
+class Talo:
 
-h.siirry_kerrokseen(5)
-h.siirry_kerrokseen(0)
+    hissien_lukumaara = 0
+
+    def __init__(self, alin, ylin, hissit):
+        self.hissit = []
+        self.alin = alin
+        self.ylin = ylin
+        self.hissien_lukumaara = hissit
+        for hissi in self.hissien_lukumaara:
+            self.hissit.append(hissi = Hissi())
+
+    def aja_hissiä(self, hissi, kerros):
+        hissi.siirry_kerrokseen(kerros)
+
+T = Talo(0, 5, 2)
+
+T.aja_hissiä(1, 5)
+T.aja_hissiä(0, 4)
--- Osio 10/2.py
+++ Osio 10/2.py
@@ -1,19 +1,17 @@
-import random
 
 class Hissi:
 
-    def __init__(self, alin_kerros):
-        self.kerros = alin_kerros
+    def __init__(self, alin, ylin):
+        self.alin = alin
+        self.ylin = ylin
+        self.kerros = self.alin
 
-    def siirry_kerrokseen(self, hissi, kerros):
-        while True:
-            if hissi.kerros == kerros:
-                break
-            num = random.randint(1,2)
-            if num == 1:
-                Talo.hissit[hissi].kerros_ylos()
-            elif num == 2:
-                Talo.hissit[hissi].kerros_alas()
+    def siirry_kerrokseen(self, kerros):
+        while self.kerros == kerros:
+            if self.kerros < kerros:
+                self.kerros_ylos()
+            elif self.kerros > kerros:
+                self.kerros_alas()
 
 
     def kerros_ylos(self):
@@ -24,43 +22,7 @@ class Hissi:
         self.kerros -= 1
         print(f"Hissi on kerroksessa {self.kerros}")
 
-class Talo:
+h = Hissi(0, 5)
 
-    def __init__(self, ylin, alin, hissi_maara):
-        self.hissit = []
-        self.ylin_kerros = ylin
-        self.alin_kerros = alin
-        self.hissien_maara = hissi_maara
-        for hissi in range(self.hissien_maara):
-            hissi = Hissi(self.alin_kerros)
-            self.hissit.append(hissi)
-
-    def aja_hissia(hissi, kerros):
-        if kerros <= Talo.hissit[hissi].ylin_kerros or kerros >= Talo.hissit[hissi].alin_kerros:
-            Hissi.siirry_kerrokseen(hissi, kerros)
-        
-    
-while True:
-    ylin = int(input("Mikä on ylin kerros: "))
-    alin = int(input("Mikä on alin kerros: "))
-    if ylin > alin:
-        break
-    else:
-        print("ylemmän kerroksen pitää olla alemman korkeamalla!")
-
-h_maara = int(input("Mikä on hissien määrä: "))
-
-talo = Talo(ylin, alin, h_maara)
-
-while True:
-    mika_hissi = int(input("Mitä hissiä haluat ajaa (Hissin numero 1 - ...): "))
-    mika_hissi -= 1
-    if 0 <= mika_hissi < len(talo.hissit) :
-        while True:
-            kerros = int(input("Hissi on alemmalla kerrosksella. Mihin kerrokseen haluat liikkua: "))
-            if kerros >= alin and kerros <= ylin:
-                break
-            print("Kerros ei löytyny!")
-        Talo.aja_hissia(mika_hissi, kerros)
-    else:
-        print("Hissiä ei löydetty")
+h.siirry_kerrokseen(5)
+h.siirry_kerrokseen(0)
